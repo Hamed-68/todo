2fb82f9da07133ff739b5371f91bcad6276168cc
Hamed-68 <hamedbakhti68@yahoo.comm>
2022-06-13 07:15:24 +0000

    add pythonanywhere to allowed host

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4emeh-h@qqe2nzw@3fml@^xry38=nng-npw!7m$yp#o__$ni)c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hamed68.pythonanywhere.com']
 
 
 # Application definition
