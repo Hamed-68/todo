c7a5777b1edcdd488834edb168d267d16665fb39
Hamed-68 <hamedbakhti918@yahoo.com>
2022-06-13 19:25:25 +0430

    add LoginRequired to list and detail views

--- task/views.py
+++ task/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from task.models import Task
 from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 # ------------------------ TASK LIST -----------------------------------
-class TaskListView(ListView):
+class TaskListView(LoginRequiredMixin, ListView):
     template_name = 'task/home.html'
 
     def get_queryset(self):
@@ -16,7 +18,7 @@ class TaskListView(ListView):
         return queryset
 
 # ------------------------ TASK LIST -----------------------------------
-class DetailTask(DetailView):
+class DetailTask(LoginRequiredMixin, DetailView):
     template_name = 'task/detail.html'
 
     def get_queryset(self):
