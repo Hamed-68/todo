f789837a5de8db2b99634266c581fcef16023ef6
Hamed-68 <hamedbakhti918@yahoo.com>
2022-06-15 17:09:12 +0430

    create searchform and add widgets fild to taskform

--- task/forms.py
+++ task/forms.py
@@ -1,9 +1,19 @@
+from calendar import Calendar
+from logging import PlaceHolder
 from django import forms
 from task.models import Task
+from django.contrib.admin.widgets import AdminDateWidget
 
 
 class TaskForm(forms.ModelForm):
-
+    
     class Meta():
         model = Task
-        exclude = ('user', 'created', 'modified', 'passed')
+        fields = ('title', 'notes', 'priority', 'due_date')
+        widgets = {
+            'due_date': AdminDateWidget(attrs={'placeholder': 'YYYY-MM-DD'})
+        }
+
+
+class SearchForm(forms.Form):                           # for filter tasks by date
+    date = forms.DateField(widget=AdminDateWidget())
