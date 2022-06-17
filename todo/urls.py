b12b470260b01311bbab218e3cea79146fddcb70
Hamed-68 <hamedbakhti918@yahoo.com>
2022-06-17 22:31:57 +0430

    add auth ruls

--- todo/urls.py
+++ todo/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo/', include('task.urls')),
+    path('', include('task.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
