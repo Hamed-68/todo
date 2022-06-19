df4780412801764d971fe7dd9253c81f76e3387c
Hamed-68 <hamedbakhti918@yahoo.com>
2022-06-19 15:26:32 +0430

    change foreignkey to onetoonekeyfield

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                            on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='profile_photo', blank=True)
 
     def __str__(self):
