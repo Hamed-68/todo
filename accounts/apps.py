b901f6b593655facb11d154d5ca55d63866efd36
Hamed-68 <hamedbakhti918@yahoo.com>
2022-06-21 00:55:56 +0430

    create signal to delete old profil photo

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from django.core.signals import request_finished
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+    def ready(self):
+        from accounts import signals
+        request_finished.connect(signals.del_old_photo)
