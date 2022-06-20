e82bf14d43052d06e39fded4bd59cef43d1a9119
Hamed-68 <hamedbakhti918@yahoo.com>
2022-06-21 00:57:25 +0430

    add media_root and media_url

--- todo/settings.py
+++ todo/settings.py
@@ -130,3 +130,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_ROOT = BASE_DIR/'media/'
+MEDIA_URL = 'media/'
