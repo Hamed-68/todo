c0dd349f785e123c6a1e7351dabb3edef7ace079
Hamed-68 <hamedbakhti918@yahoo.com>
2022-07-01 12:08:29 +0430

    edit logout name

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout/'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('password_reset/',
