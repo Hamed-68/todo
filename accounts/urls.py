53830a84d312eba010f371baa519e4de7d7f36f0
Hamed-68 <hamedbakhti918@yahoo.com>
2022-07-01 16:24:33 +0430

    change success_url in 'changepasswordview'

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,9 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/',
+        auth_views.PasswordChangeView.as_view(success_url = reverse_lazy('accounts:password_change_done')),
+        name='password_change'),
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('password_reset/',
         auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')),
